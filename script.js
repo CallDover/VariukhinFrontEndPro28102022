2262b707ec0a1f4bf01a7e84d4cc21003a043cd9
CallDover <dimavaruhin04@gmail.com>
2022-10-28 16:22:01 +0200

    homework1 done

---
+++ script.js
@@ -0,0 +1,7 @@
+const text1 = prompt('Put some text here','');
+const text2 = prompt('Put some text here','');
+const text3 = prompt('Put some text here','');
+const bigNum = 12345 ;
+alert(text1+text2+text3);
+const numString = String(bigNum);
+alert(`Numbers: ${numString[0]} ${numString[1]} ${numString[2]} ${numString[3]} ${numString[4]}`)
