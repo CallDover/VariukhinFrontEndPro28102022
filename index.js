01e2cebabcdbd58ac3cd8ee2b54c21cafb97e294
CallDover <dimavaruhin04@gmail.com>
2022-11-22 01:58:40 +0100

    homework 12 done2

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+function generateKey(length, symbols) {
+    let result = "";
+    while (result.length < length) {
+        let random = Math.floor(Math.random() * symbols.length);
+        result += symbols[random];
+    }
+    return result;
+}
+
 const key = generateKey(16, characters);
-console.log(key); // eg599gb60q926j8i
+console.log(key); 
