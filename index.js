8ece8d10c113e9a30759f8be84e147cb5e4316c9
CallDover <dimavaruhin04@gmail.com>
2023-01-25 17:59:29 +0100

    hw20 done

---
+++ index.js
@@ -0,0 +1,17 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+        return this;
+    }
+};
+
+ladder.up().up().down().up().up().showStep(); // 1
